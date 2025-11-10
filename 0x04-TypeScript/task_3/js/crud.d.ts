ed6ce7789d85ee721f9d68db6aa2427d97763096
Ze-aman <aman09dida@gmail.com>
2025-11-10 11:23:07 +0300

    Modify insertRow to return a number type
    Updated insertRow function declaration to return a number.

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -3,5 +3,8 @@ import { RowID, RowElement } from './interface';
 
 // Declare the functions available in crud.js
 declare function insertRow(row: RowElement): RowID;
+declare function insertRow(row: RowElement): number;
 declare function deleteRow(rowId: RowID): void;
 declare function updateRow(rowId: RowID, row: RowElement): RowID;
+
+
